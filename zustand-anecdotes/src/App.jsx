c6158c109f226db2f614c2523a13e70bfdf26d6f
Gilbertirumva <167775136+GilbertIrumva@users.noreply.github.com>
2026-07-21 17:05:44 +0300

    completed exercise 6.7 anecdotes, step6

--- zustand-anecdotes/src/App.jsx
+++ zustand-anecdotes/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
+import { useAnecdoteActions } from './store'
+
 import Filter from './components/Filter'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
+  const actions = useAnecdoteActions()
+
+  useEffect(() => {
+    actions.initializeAnecdotes()
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
