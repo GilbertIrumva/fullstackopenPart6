7cf03806495078129db65e3229229e78e29acf6c
Gilbertirumva <167775136+GilbertIrumva@users.noreply.github.com>
2026-07-22 14:24:48 +0300

    completion of exercise 6.16 anecdotes, step15

--- zustand-anecdotes/src/App.jsx
+++ zustand-anecdotes/src/App.jsx
@@ -1,29 +1,39 @@
-import { useEffect } from 'react'
-import { useAnecdoteActions } from './store'
+import { useQuery } from '@tanstack/react-query'
 
-import Filter from './components/Filter'
-import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
-import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-  const actions = useAnecdoteActions()
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
 
-  useEffect(() => {
-    actions.initializeAnecdotes()
-  }, [])
+  if (result.isPending) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return (
+      <div>
+        anecdote service not available due to
+        problems in server
+      </div>
+    )
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
-      <h2>Anecdotes</h2>
+      <h3>Anecdote app</h3>
 
-      <Notification />
-
-      <Filter />
-
-      <AnecdoteList />
-
-      <AnecdoteForm />
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>has {anecdote.votes} votes</div>
+        </div>
+      ))}
     </div>
   )
 }
